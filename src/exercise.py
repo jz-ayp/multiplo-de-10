eeb05190bb06bb9d2735a1dff2559a8231bd9272
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2021-02-24 23:10:34 -0600

    Update exercise.py
    Delete solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,19 +1,7 @@
 def main():
-    # delete the 'pass' statement, then begin coding below:
-    # p ass
-    
-    # Entradas
-    numero = int(input("Introduzca un número: "))
-    
-    # Proceso
-    if numero % 10 == 0:
-        resultado = "sí"
-    else:
-        resultado = "no"
-        
-    # Salidas
-    print("El número", numero, resultado, "es múltiplo de 10")
-    
+    # borra la instrucción 'pass' y escribe tu código aquí abajo:
+    pass
+
 
 if __name__ == '__main__': 
     main()
