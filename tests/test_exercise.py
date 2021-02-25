cea7979f1be93a76f17dd0ba53651c044660ba46
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2021-02-24 23:00:21 -0600

    Update test_exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,14 +1,17 @@
 import pytest
 import src.exercise
 
-inp_1 = []
-out_1 = []
+inp_1 = ['123']
+out_1 = ['El número 123 no es múltiplo de 10']
 
-inp_2 = []
-out_2 = []
+inp_2 = ['24560']
+out_2 = ['El número 24560 sí es múltiplo de 10']
+
+inp_3 = ['102030']
+out_3 = ['El número 102030 sí es múltiplo de 10']
 
 # run the test function for each input/output pair
-@pytest.mark.parametrize("test_input, expected", [(inp_1, out_1), (inp_2, out_2)])
+@pytest.mark.parametrize("test_input, expected", [(inp_1, out_1), (inp_2, out_2), (inp_3, out_3)])
 def test_capture_stdout(capsys, test_input, expected):
     
     # Load the test input for the program execution:
