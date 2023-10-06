508acde4df7190831e4599a0283398f417e47da2
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2023-10-06 01:05:36 -0600

    Remove solution

--- multiplo_de_10.py
+++ multiplo_de_10.py
@@ -1,14 +1,15 @@
 """
-Determinar si un número es múltiplo de 10
+Inserta el encabezado aquí y escribe tu código abajo
 """
 
+# Declaraciones
+CONSTANTE = valor
+
 # Entradas
-numero = eval(input("Introduzca un número: "))
-	
+entrada = input()
+
 # Proceso
-if numero % 10 == 0:
-    resultado = "sí"
-else:
-	resultado = "no"
+
+
 # Salidas
-print("El número", numero, resultado, "es múltiplo de 10.")
+print(salida)
